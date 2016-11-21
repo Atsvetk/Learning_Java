dcb6cc69fa1b25309e4660c344fa796a1575646d
Atsvetk <alexandra.cvetkova@gmail.com>
2016-11-22 00:36:47 +0300

    adding 4_2

---
+++ src/problem_4_2.java
@@ -0,0 +1,14 @@
+public class problem_4_2 {
+    public static void main(String[] args) {
+        int[] A = new int[]{1, -5, 6, 4, 0, 1, -1};
+        int sum = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+        }
+        float s = sum;
+        float average = s/A.length;
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+
+    }
+}
