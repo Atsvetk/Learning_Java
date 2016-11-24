3722dc8c770ca39b29c7f936449d9917b89d24f1
Atsvetk <alexandra.cvetkova@gmail.com>
2016-11-24 14:25:44 +0300

    Odd or even number

---
+++ src/problem_1_1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class problem_1_1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Введите число: ");
+		if (scan.hasNextInt()) {
+			int n = scan.nextInt();
+			if (n%2==0) {
+				System.out.println("Чётное!");
+			} else {
+				System.out.println("Нечётное!");
+			}
+		} else {
+			System.out.println("Нужно было ввести целое число!");
+		}
+		scan.close();
+	}
+}
