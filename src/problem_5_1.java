1d8d2e1f956f00fda08d452e7afe01759888eb3b
Atsvetk <alexandra.cvetkova@gmail.com>
2016-11-29 18:25:51 +0300

    Delete unnecessary variables and brackets.
    Delete unnecessary variables (err, number) and brackets.

--- src/problem_5_1.java
+++ src/problem_5_1.java
@@ -2,19 +2,16 @@ import java.util.Scanner;
 
 public class problem_5_1 {
     public static void main(String[] args) {
+        
         Scanner scan = new Scanner(System.in);
-        System.out.println("Input 0 < N < 30: ");
+        System.out.println("Input 0 <= N <= 30: ");
         int n = scan.nextInt();
-        int number = 1;
-
-        boolean err = false;
-        if ((n < 0) || (n > 30)) {
+        
+        if (n < 0 || n > 30) {
             System.out.println("Wrong N!");
-            err = true;
-        }
-        if (!err) {
-            number = number << n;
-            System.out.println("2 raised to the power " + n + " equals " + number);
+            System.exit(0);
+        } else {
+            System.out.println("2 raised to the power " + n + " equals " + (1 << n));
         }
     }
 }
