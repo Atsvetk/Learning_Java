3a2928b43eedf7ad57da23846a440d7c00f2c9b1
Atsvetk <alexandra.cvetkova@gmail.com>
2016-11-29 20:25:42 +0300

    Randomizing and printing array

--- src/Problem4_3.java
+++ src/Problem4_3.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Problem4_3 {
     public static void main(String[] args) {
-        int[] A = new int[]{1, -5, 6, 4, 0, 1, -1};
-        boolean flag = false;
+        int[] arr = new int[25];
+        Random rand = new Random();
+        System.out.print("{ ");
+        for (int i = 0; i < 25; i++) {
+        	arr[i] = (rand.nextInt(101) - 50);
+        	System.out.print(arr[i] + " ");
+        }
+        System.out.print("}\n");
         Scanner scan = new Scanner(System.in);
         System.out.print("Input the desired number: ");
         int a = scan.nextInt();
-        for (int i = 0; i < A.length; i++) {
-            if (A[i]==a) {
-                System.out.println("Found number " + a + " in the " + (i+1) + " place");
-                flag = true;
-                break;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == a) {
+                System.out.println("Found number " + a + " in the " + (i + 1) + " place");
+                System.exit(0);
             }
         }
-        if (!flag) {
-            System.out.println("There is no number " + a + " in the array.");
-        }
+        System.out.println("There is no number " + a + " in the array.");
     }
 }
