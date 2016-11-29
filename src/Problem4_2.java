5ce2ea653331bf6e462924e5626eda52e852da8f
Atsvetk <alexandra.cvetkova@gmail.com>
2016-11-29 20:20:16 +0300

    Randomizing array

--- src/Problem4_2.java
+++ src/Problem4_2.java
@@ -1,12 +1,21 @@
+import java.util.Random;
+
 public class Problem4_2 {
     public static void main(String[] args) {
-        int[] A = new int[]{1, -5, 6, 4, 0, 1, -1};
+        int[] arr = new int[25];
+        Random rand = new Random();
+        System.out.print("{ ");
+        for (int i = 0; i < 25; i++) {
+        	arr[i] = (rand.nextInt(101) - 50);
+        	System.out.print(arr[i] + " ");
+        }
+        System.out.print("}\n");
         int sum = 0;
-        for (int i = 0; i < A.length; i++) {
-            sum += A[i];
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
         }
         float s = sum;
-        float average = s/A.length;
+        float average = s / arr.length;
         System.out.println("Sum = " + sum);
         System.out.println("Average = " + average);
 
