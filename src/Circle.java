9696a1ce8c0c5ad9e7743b578c6dfa608ae3d3d2
Atsvetk <alexandra.cvetkova@gmail.com>
2016-12-05 21:53:27 +0300

    Refactoring constructor & isInCircle method

--- src/Circle.java
+++ src/Circle.java
@@ -4,14 +4,11 @@ public class Circle {
     double r;
 
     public Circle(double r) {
-        if(r <= 0) {
-            throw new RuntimeException("Радиус должен быть положительным");
-        }
-        this.r = r;
+        this(0,0,r);
     }
 
     public Circle(double x, double y, double r) {
-        if(r <= 0) {
+        if (r <= 0) {
             throw new RuntimeException("Радиус должен быть положительным");
         }
         this.x = x;
@@ -39,28 +36,24 @@ public class Circle {
         this.r *= rX;
     }
 
-    public boolean isInCircle(double x0, double y0){
-        if (Math.pow(this.x - x0, 2) + Math.pow(this.y - y0, 2) <= Math.pow(this.r, 2)){
-            return true;
-        } else {
-            return false;
-        }
+    public boolean isInCircle(double x0, double y0) {
+        return (Math.pow(this.x - x0, 2) + Math.pow(this.y - y0, 2) - Math.pow(this.r, 2) <= 1e-8);
     }
 
-    public double length(){
+    public double length() {
         return 2 * Math.PI * this.r;
     }
 
-    public double square(){
+    public double square() {
         return Math.PI * this.r * this.r;
     }
 
-    public double diameter(){
+    public double diameter() {
         return this.r * 2;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "x = " + this.x + "; y = " + this.y + "; r = " + this.r;
     }
 
