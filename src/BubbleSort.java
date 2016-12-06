07897a6272c63afc5bfcf11e7cd44ba2a4c2da7d
Atsvetk <alexandra.cvetkova@gmail.com>
2016-12-06 19:14:22 +0300

    Add Bubble Sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = new int[25];
+        Random rand = new Random();
+        System.out.print("{ ");
+        for (int i = 0; i < 25; i++) {
+            arr[i] = (rand.nextInt(101) - 50);
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print("}\n");
+
+        boolean flag = true;
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tmp;
+                    flag = true;
+                }
+            }
+        }
+        System.out.print("{ ");
+        for (int i = 0; i < 25; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print("}");
+
+    }
+}
+
